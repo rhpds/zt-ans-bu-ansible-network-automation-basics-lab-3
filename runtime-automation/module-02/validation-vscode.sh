5c1e88aa4181c353f5caf7149de909e1f3657798
Mitesh Sharma <mitsharm@redhat.com>
2025-09-05 20:13:00 +0530

    update

--- runtime-automation/module-02/validation-vscode.sh
+++ runtime-automation/module-02/validation-vscode.sh
@@ -4,8 +4,10 @@ set -euxo pipefail
 
 if [ ! -f /home/rhel/resource.yml ]; then
     echo "No file was found at /home/rhel/resource.yml"
+    exit 1
 fi
 
-if ! grep "snmp_server" /home/rhel/resource.yml; then
+if ! grep "cisco.ios.snmp_server:" /home/rhel/resource.yml; then
     echo "The resource.yml playbook doesn't contain the snmp_server module"
+    exit 1
 fi
